667bf13c2105dbf9a79238f2e39840005cc132a0
joonatan40 <104058964+joonatan40@users.noreply.github.com>
2022-04-20 14:12:54 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -21,13 +21,27 @@ async function addDistrictsGeoJson(url) {
 }
 addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
 
+function createCircle(feature, latlng) {
+ let options = {
+ radius: 5,
+ fillColor: 'red',
+ fillOpacity: 0.5,
+ color: 'red',
+ weight: 1,
+ opacity: 1,
+ }
+ return L.circleMarker(latlng, options)
+}
 async function addCelltowersGeoJson(url) {
  const response = await fetch(url)
  const data = await response.json()
- const markers = L.geoJson(data)
- markers.addTo(map)
+ const circles = L.geoJson(data, {
+ pointToLayer: createCircle,
+ })
+ circles.addTo(map)
 }
 addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson')
+
 
 
 function getColor(property) {
